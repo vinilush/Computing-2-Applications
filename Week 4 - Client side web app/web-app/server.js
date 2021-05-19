7d4cf877bbdad17dbedc365a9bc81552cbedb91d
vinilush <lb1620@ic.ac.uk>
2021-05-19 11:34:20 +0100

    changes to open presentation webapp

--- Week 4 - Client side web app/web-app/server.js
+++ Week 4 - Client side web app/web-app/server.js
@@ -3,7 +3,7 @@ import express from "express";
 const port = 8080;
 const app = express();
 
-app.use("/", express.static("web-app/static"));
+app.use("/", express.static("web-app/static/presentation"));
 
 app.listen(port, function () {
     console.log(`Listening on port ${port} – http://localhost:${port}`);
